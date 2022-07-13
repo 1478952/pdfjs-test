0d0a06792853f9982f841a983bafadad743e5457
1478952 <1478952@naver.com>
2022-07-13 22:39:19 +0900

    32px add

--- web/zoom.js
+++ web/zoom.js
@@ -58,7 +58,7 @@ function enablePinchZoom(pdfViewer) {
       PDFViewerApplication.pdfViewer.currentScale = newPinchScale;
       const rect = container.getBoundingClientRect();
       const dx = startX - rect.left;
-      const dy = startY - rect.top;
+      const dy = startY - rect.top + 32;
       container.scrollLeft += dx * (pinchScale - 1);
       container.scrollTop += dy * (pinchScale - 1);
     } else {
